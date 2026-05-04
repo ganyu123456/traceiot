cd6394b906a0772867a94fae2b501e4b0595a9a0
ganyu123456 <1741478590@qq.com>
2026-05-04 12:19:05 +0800

    提交

--- backend/src/TraceIot.Application/TraceIotApplicationModule.cs
+++ backend/src/TraceIot.Application/TraceIotApplicationModule.cs
@@ -1,4 +1,6 @@
+using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.Application;
+using Volo.Abp.AutoMapper;
 using Volo.Abp.Identity;
 using Volo.Abp.Modularity;
 
@@ -6,10 +8,20 @@ namespace TraceIot;
 
 [DependsOn(
     typeof(AbpDddApplicationModule),
+    typeof(AbpAutoMapperModule),
     typeof(AbpIdentityApplicationModule),
     typeof(TraceIotApplicationContractsModule),
     typeof(TraceIotDomainModule)
 )]
 public class TraceIotApplicationModule : AbpModule
 {
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        context.Services.AddAutoMapperObjectMapper<TraceIotApplicationModule>();
+
+        Configure<AbpAutoMapperOptions>(options =>
+        {
+            options.AddMaps<TraceIotApplicationModule>(validate: false);
+        });
+    }
 }
